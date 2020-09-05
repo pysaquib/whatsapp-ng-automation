6720770b8a9735cb77e15c3ba60875488f88a6f0
nilam1996 <nilam@navgurukul.org>
2020-09-05 13:28:09 +0530

    Updated Api

--- getContacts.js
+++ getContacts.js
@@ -3,7 +3,7 @@ module.exports = async function getContacts() {
     const axios = require('axios');
     let contacts = {}
 
-    await axios.get('https://sheetdb.io/api/v1/dlk07s7isr190')
+    await axios.get('https://sheetdb.io/api/v1/ybynjbo13n0bk')
         .then((res) => {
             // console.log(res.data);
             contacts = res.data;
